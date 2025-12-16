fc7e412ac039b40e945b3e249b1d5357aff860bd
sajal3289beai-lab <sajal3289beai.chitkara.edu.in>
2025-12-16 19:43:45 +0530

    Added

--- username.py
+++ username.py
@@ -3,18 +3,20 @@ if login.lower()=="sign up":
     username=input("Enter your username: ")
     password=input("Enter your password: ")
     with open("file.txt","a") as file:
-     file.write(username+":"+password +"\n")
+       file.write(username+":"+password +"\n")
     print("succesfull")
 elif login.lower()=="login":
    username=input("enter your username:")
    password=input("enter your password:")
+   found=False
    with open("file.txt","r") as file:
       for line in file:
          username1,password1=line.strip().split(":")
          if username1==username and password1==password:
             print("login successfull")
-         else:
+            found=True
+            break
+      if not found:
             print("incorrect username or password")
-            #1234
 else:
     print("invalid option")   
