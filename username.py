941e3da60c5448adb98bdb0819f32174aef69b03
sajal3289beai-lab <sajal3289beai.chitkara.edu.in>
2025-12-16 19:23:56 +0530

    added

---
+++ username.py
@@ -0,0 +1,20 @@
+login=input("Enter login or sign up: ")
+if login.lower()=="sign up":
+    username=input("Enter your username: ")
+    password=input("Enter your password: ")
+    with open("file.txt","a") as file:
+     file.write(username+":"+password +"\n")
+    print("succesfull")
+elif login.lower()=="login":
+   username=input("enter your username:")
+   password=input("enter your password:")
+   with open("file.txt","r") as file:
+      for line in file:
+         username1,password1=line.strip().split(":")
+         if username1==username and password1==password:
+            print("login successfull")
+         else:
+            print("incorrect username or password")
+            #1234
+else:
+    print("invalid option")
